4bdd19f379ee62ebc1d03fe3adc58d0a065abd7f
layla-lili <laylaisa.211@gmail.com>
2021-03-08 18:51:48 +0300

    FE signin signout

---
+++ src/store/actions/types.js
@@ -0,0 +1,10 @@
+// Product Types
+
+
+//auth
+export const SIGN_UP = "SIGN_UP";
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+
+// Auth Types
+export const SET_USER = "SET_USER";
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,20 @@
+import * as types from "../actions/types";
+
+const initialState = {
+  user: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.SET_USER:
+      console.log(action.payload);
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
