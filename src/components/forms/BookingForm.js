82377c76e9daf518bb1376e2650d3c1a7d582e3e
layla-lili <laylaisa.211@gmail.com>
2021-03-14 15:57:30 +0300

    booking Form

--- src/components/forms/BookingForm.js
+++ src/components/forms/BookingForm.js
@@ -4,100 +4,132 @@ import { useHistory } from "react-router";
 function BookingForm() {
   const history = useHistory();
 
-  const [passenger, setPassenger] = useState({
-    firstName: "",
-    lastName: "",
-    // cpr: "",
-    ageGroup: ["Infant", "Children", "Young", "Adult", "Elders"],
-    // gender: ["male", "female"],
-    // phone: "",
+  const [booking, setbooking] = useState({
+    depAirport: "WWWAirport",
+    depDate: "2021-03-15",
+    depTime: "15:00",
+    arrAirport: "DDDAirport",
+    arrDate: "2021-03-15",
+    arrTime: "17:00",
+    economy: 20,
+    business: 10,
+    price: 280,
   });
 
   const handleChange = (event) => {
-    setPassenger({ ...passenger, [event.target.name]: event.target.value });
+    setbooking({ ...booking, [event.target.name]: event.target.value });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    history.push("/passengers");
+    history.push("/passenger");
   };
 
   return (
     <form className="container" onSubmit={handleSubmit}>
-      <label className="form-label">
-        <h2>Passenger1</h2>
-      </label>
-      {/* <h1>{foundPassenger ? "Update" : "Create"} Passenger</h1> */}
-      <div className="mb-3">
-        <label className="form-label">First Name</label>
-        <input
-          type="text"
-          value={passenger.firstName}
-          onChange={handleChange}
-          name=" firstName"
-          className="form-control"
-        />
-      </div>
-      <div className="mb-3">
-        <label className="form-label">Last Name</label>
-        <input
-          type="text"
-          value={passenger.lastName}
-          onChange={handleChange}
-          name=" lastName"
-          className="form-control"
-        />
-      </div>
-      {/* <div className="mb-3">
-        <label className="form-label">CPR</label>
-        <input
-          type="text"
-          value={passenger.cpr}
-          onChange={handleChange}
-          name=" cpr"
-          className="form-control"
-        />
-      </div> */}
-      <div className="mb-3">
-        <label className="form-label">Age Group</label>
-        <select
-          name="ageGroup"
-          onChange={handleChange}
-          className="form-control"
-        >
-          {passenger.ageGroup.map((p) => (
-            <option selected={passenger.ageGroup === p} value={p}>
-              {p}
-            </option>
-          ))}
-        </select>
-      </div>
-      {/* <div className="mb-3">
-        <label className="form-label">Gender</label>
-        <select name="gender" onChange={handleChange} className="form-control">
-          {passenger.gender.map((p) => (
-            <option selected={passenger.gender === p} value={p}>
-              {p}
-            </option>
-          ))}
-        </select>
-      </div>
+      <fieldset disabled="disabled">
+        <label className="form-label">
+          <h2>Booking Form</h2>
+        </label>
+        {/* <h1>{foundPassenger ? "Update" : "Create"} Passenger</h1> */}
+        <div className="mb-3">
+          <label className="form-label">Departure Airport</label>
+          <input
+            type="text"
+            value={booking.depAirport}
+            onChange={handleChange}
+            name="depAirport"
+            className="form-control"
+          />
+        </div>
 
-      <div className="mb-3">
-        <label className="form-label">Phone</label>
-        <input
-          type="text"
-          value={passenger.phone}
-          onChange={handleChange}
-          name=" phone"
-          className="form-control"
-        />
-      </div> */}
+        <div className="mb-3">
+          <label className="form-label">Departure Date</label>
+          <input
+            type="text"
+            value={booking.depDate}
+            onChange={handleChange}
+            name="depDate"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Departure Time</label>
+          <input
+            type="text"
+            value={booking.depTime}
+            onChange={handleChange}
+            name="depTime"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Arrival Airport</label>
+          <input
+            type="text"
+            value={booking.arrAirport}
+            onChange={handleChange}
+            name="arrAirport"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Arrival Date</label>
+          <input
+            type="text"
+            value={booking.arrDate}
+            onChange={handleChange}
+            name="arrDate"
+            className="form-control"
+          />
+        </div>
 
+        <div className="mb-3">
+          <label className="form-label">Arrival Time</label>
+          <input
+            type="text"
+            value={booking.arrTime}
+            onChange={handleChange}
+            name="arrTime"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Economy </label>
+          <input
+            type="text"
+            value={booking.economy}
+            onChange={handleChange}
+            name="economy"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Business</label>
+          <input
+            type="text"
+            value={booking.business}
+            onChange={handleChange}
+            name="business"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Price</label>
+          <input
+            type="text"
+            value={booking.price}
+            onChange={handleChange}
+            name="price"
+            className="form-control"
+          />
+        </div>
+      </fieldset>
       <button type="submit" className="btn btn-info float-right">
         {/* {foundPassenger ? "Update" : "Create"} */}
-        Book
+        Next
       </button>
     </form>
   );
---
+++ src/components/forms/PassengerForm.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+
+function PassengerForm() {
+  const history = useHistory();
+
+  const [passenger, setPassenger] = useState({
+    firstName: "",
+    lastName: "",
+    // cpr: "",
+    ageGroup: ["Infant", "Children", "Young", "Adult", "Elders"],
+    // gender: ["male", "female"],
+    // phone: "",
+  });
+
+  const handleChange = (event) => {
+    setPassenger({ ...passenger, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    history.push("/passenger");
+  };
+
+  return (
+    <form className="container" onSubmit={handleSubmit}>
+      <label className="form-label">
+        <h2>Passenger1</h2>
+      </label>
+      {/* <h1>{foundPassenger ? "Update" : "Create"} Passenger</h1> */}
+      <div className="mb-3">
+        <label className="form-label">First Name</label>
+        <input
+          type="text"
+          value={passenger.firstName}
+          onChange={handleChange}
+          name=" firstName"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Last Name</label>
+        <input
+          type="text"
+          value={passenger.lastName}
+          onChange={handleChange}
+          name=" lastName"
+          className="form-control"
+        />
+      </div>
+      {/* <div className="mb-3">
+        <label className="form-label">CPR</label>
+        <input
+          type="text"
+          value={passenger.cpr}
+          onChange={handleChange}
+          name=" cpr"
+          className="form-control"
+        />
+      </div> */}
+      <div className="mb-3">
+        <label className="form-label">Age Group</label>
+        <select
+          name="ageGroup"
+          onChange={handleChange}
+          className="form-control"
+        >
+          {passenger.ageGroup.map((p) => (
+            <option selected={passenger.ageGroup === p} value={p}>
+              {p}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* <div className="mb-3">
+        <label className="form-label">Gender</label>
+        <select name="gender" onChange={handleChange} className="form-control">
+          {passenger.gender.map((p) => (
+            <option selected={passenger.gender === p} value={p}>
+              {p}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Phone</label>
+        <input
+          type="text"
+          value={passenger.phone}
+          onChange={handleChange}
+          name=" phone"
+          className="form-control"
+        />
+      </div> */}
+
+      <button type="submit" className="btn btn-info float-right">
+        {/* {foundPassenger ? "Update" : "Create"} */}
+        Book
+      </button>
+    </form>
+  );
+}
+
+export default PassengerForm;
